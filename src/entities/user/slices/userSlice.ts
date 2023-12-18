3ee4b9fc0f6ceb1064503f3ded747c90f3002725
Vadym <124910250+plesh9@users.noreply.github.com>
2023-12-19 01:35:24 +0200

    +config for redux

---
+++ src/entities/user/slices/userSlice.ts
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { RootState } from 'app/store';
+
+interface UserState {
+  name: string;
+}
+
+const initialState: UserState = {
+  name: 'Vadym',
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    setName: (state) => {
+      state.name = 'Dima';
+    },
+  },
+});
+
+export const { setName } = userSlice.actions;
+
+export const selectUserName = (state: RootState) => state.user.name;
+
+export default userSlice.reducer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,21 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTES } from 'app/router/utils';
+import { useAppSelector } from 'app/store/hooks';
+import { selectUserName } from 'entities/user/slices/userSlice';
 
 interface MainPageProps {}
 
 export const MainPage: FC<MainPageProps> = () => {
+  const userName = useAppSelector(selectUserName);
+
   return (
     <div>
       <Link to={ROUTES.games.roulette}>Roulette</Link>
       <Link to={ROUTES.games.slots}>Slots</Link>
       <Link to={ROUTES.games.hummer}>Hummer</Link>
+
+      <div>{userName}</div>
     </div>
   );
 };
